d79d5f1497bff4b033c616a11650b472aaead11f
@Draichi <lucasdraichi@gmail.com>
2021-11-10 02:34:57 -0300

    feat(pages): adds notifications and settings

---
+++ src/pages/notifications.tsx
@@ -0,0 +1,9 @@
+import Layout from "components/template/Layout";
+
+export default function Notifications() {
+  return (
+    <Layout title="Notification" subtitile="Working in progress">
+      <h3>Yeah boi</h3>
+    </Layout>
+  );
+}
